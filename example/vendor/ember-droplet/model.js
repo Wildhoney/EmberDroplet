c3b728842e05536e65d8b1960b6e9c8491ffa189
Wildhoney <adam.timberlake@gmail.com>
2015-08-11 22:56:10 +0100

    Added CRUD methods

---
+++ example/vendor/ember-droplet/model.js
@@ -0,0 +1,16 @@
+/**
+ * @method DropletModel
+ * @constructor
+ */
+export default class DropletModel {
+
+    /**
+     * @method setType
+     * @param {Number} statusType
+     * @return {void}
+     */
+    setType(statusType) {
+        this.statusType = Number(statusType);
+    }
+
+}
