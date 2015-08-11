821b6a5f82f7b0ff9cc7b5e2b934093c9575d8dd
Wildhoney <adam.timberlake@gmail.com>
2015-08-11 20:05:19 +0100

    Started mixin

---
+++ example/vendor/ember-droplet/mixin.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+/**
+ * @module EmberDroplet
+ * @author Adam Timberlake
+ * @see https://github.com/Wildhoney/EmberDroplet
+ */
+export default Ember.Mixin.create({
+
+    /**
+     * @property mimeTypes
+     * @type {Array}
+     */
+    mimeTypes: ['image/jpeg', 'image/jpg', 'image/gif', 'image/png', 'text/plain']
+
+});
